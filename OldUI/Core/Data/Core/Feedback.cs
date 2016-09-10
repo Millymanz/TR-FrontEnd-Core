b113d2478e3f0f36bf387fd09b6c5c99a1f742ca
ee01ppa <cud_joe@hotmail.com>
2016-09-10 16:11:28 +0100

    Feedback email changes

---
+++ OldUI/Core/Data/Core/Feedback.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Newtonsoft.Json;
+using TradeRiser.Core.Data;
+
+namespace TradeRiser.Core.Data
+{
+    public class Feedback : IVaultDataBound
+    {
+        [JsonProperty("id")]
+        public Int64 ID { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("firstName")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("lastName")]
+        public string LastName { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("actioned")]
+        public bool Actioned { get; set; }
+
+        [JsonProperty("insertDateTime")]
+        public DateTime InsertDateTime { get; set; }
+
+        public void Load(IReader reader)
+        {
+            this.ID = reader.Get<Int64>("ID");
+            this.Name = reader.Get<string>("Name");
+            this.FirstName = reader.Get<string>("FirstName");
+            this.LastName = reader.Get<string>("LastName");
+            this.Email = reader.Get<string>("Email");
+            this.Message = reader.Get<string>("Message");
+            this.Actioned = reader.Get<bool>("Actioned");
+            this.InsertDateTime = reader.Get<DateTime>("InsertDateTime");
+        }
+    }
+}
---
+++ OldUI/TradeRiser/Contract/AppDataAccess.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using TradeRiser.Core.Logging;
+
+
+
+namespace TradeRiser.Core.Data
+{
+    public class AppDataAccess
+    {
+        #region private fields
+
+        /// <summary>
+        /// The component.
+        /// </summary>
+        private const string Component = "TradeRiser.Core.App";
+
+        /// <summary>
+        /// The sender.
+        /// </summary>
+        private const string Sender = "AppDataAccess";
+
+        #endregion private fields
+
+        #region properties
+
+        /// <summary>
+        /// Gets the data access helper.
+        /// </summary>
+        public IDataAccess Database
+        {
+            get
+            {
+                return new DataAccess();
+            }
+        }
+
+        #endregion properties
+
+        #region public methods
+
+        /// <summary>
+        /// Creates the user.
+        /// </summary>
+        /// <param name="user">The user.</param>
+        /// <param name="userId">The user id.</param>
+        public bool SubmitFeedback(Feedback feedback)
+        {
+            bool success;
+            using (IDataAccess data = this.Database)
+            {
+                DataSettings settings = new DataSettings(CoreConnections.MembershipDirectConnection, "traderiser.FeedbacksUpdate");
+
+                //if (feedback.ID != 0)
+                //{
+                    settings.Parameters.Add(new DataParameter() { ParameterName = "@ID", Value = feedback.ID });
+                //}
+
+                settings.Parameters.Add(new DataParameter() { ParameterName = "@Name", Value = feedback.Name });
+                settings.Parameters.Add(new DataParameter() { ParameterName = "@FirstName", Value = feedback.FirstName });
+                settings.Parameters.Add(new DataParameter() { ParameterName = "@LastName", Value = feedback.LastName });
+                settings.Parameters.Add(new DataParameter() { ParameterName = "@Email", Value = feedback.Email });
+                settings.Parameters.Add(new DataParameter() { ParameterName = "@Message", Value = feedback.Message });
+                settings.Parameters.Add(new DataParameter() { ParameterName = "@Actioned", Value = feedback.Actioned });
+
+                if (feedback.InsertDateTime > DateTime.MinValue)
+                {
+                    settings.Parameters.Add(new DataParameter() { ParameterName = "@InsertDateTime", Value = feedback.InsertDateTime, DbType = DbType.DateTime });
+                }
+                else
+                {
+                    settings.Parameters.Add(new DataParameter() { ParameterName = "@InsertDateTime", Value = DBNull.Value, DbType = DbType.DateTime });
+                }
+
+                DataResult result = data.ExecuteNonQuery(settings);
+                success = result.Success;
+                if (!success)
+                {
+
+                    Log.Exception(AppDataAccess.Component,
+                        string.Format("{0}.{1}", AppDataAccess.Sender, "FeedbacksUpdate"), result.Exception);
+                    throw result.Exception;
+                }
+
+            }
+            return success;
+        }
+
+
+
+        /// <summary>
+        /// Gets all users.
+        /// </summary>
+        public List<Feedback> GetFeedbacks()
+        {
+            List<Feedback> users = null;
+
+            using (IDataAccess data = this.Database)
+            {
+                users = data.Get<Feedback>(new DataSettings(CoreConnections.MembershipDirectConnection, "traderiser.FeedbackSelect", "feedback"));
+            }
+
+            return users;
+        }
+
+        #endregion public methods
+
+    }
+}
